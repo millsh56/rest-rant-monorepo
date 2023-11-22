78ee8bb02539842e5da26a8966991c653eb0d6a6
Hannah Mills <millsh56@gmail.com>
2023-11-21 20:13:04 -0500

    trying to start cookie session

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -16,9 +16,10 @@ function LoginForm() {
     const [errorMessage, setErrorMessage] = useState(null)
 
     async function handleSubmit(e) {
-        e.preventDefault();
+        // e.preventDefault();
         const response = await fetch('http://localhost:5000/authentication', {
             method: 'POST',
+            credentials: 'include',
             headers: {
                 'Content-Type': 'application/json'
             },
